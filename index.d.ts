444d05d2e429922e90d9454765a4795840111639
3846masa <3846masahiro+git@gmail.com>
2025-01-07 16:56:03 +0900

    fix: strict type definitions for RouteParams

--- index.d.ts
+++ index.d.ts
@@ -8,21 +8,26 @@ export function parse(route: RegExp): {
 	pattern: RegExp;
 }
 
+type KeyRecord<T extends string> =
+	T extends `${infer P}?` // :id?
+		? { [K in P]?: string | undefined }
+	: T extends `${infer P}.${string}` // :id.ext
+		? { [K in P]: string }
+	: { [K in T]: string };
+
 export type RouteParams<T extends string> =
 	T extends `${infer Prev}/*/${infer Rest}`
-		? RouteParams<Prev> & { wild: string } & RouteParams<Rest>
-	: T extends `${string}:${infer P}?/${infer Rest}`
-		? { [K in P]?: string } & RouteParams<Rest>
-	: T extends `${string}:${infer P}/${infer Rest}`
-		? { [K in P]: string } & RouteParams<Rest>
-	: T extends `${string}:${infer P}?`
-		? { [K in P]?: string }
-	: T extends `${string}:${infer P}`
-		? { [K in P]: string }
-	: T extends `${string}*`
+		? RouteParams<Prev> & { wild: string } & RouteParams<`/${Rest}`>
+	: T extends `:${infer Rest}`
+		? RouteParams<`/:${Rest}`>
+	: T extends `${string}/:${infer P}/${infer Rest}`
+		? KeyRecord<P> & RouteParams<`/${Rest}`>
+	: T extends `${string}/:${infer P}`
+		? KeyRecord<P>
+	: T extends `${string}/*`
 		? { "*": string }
-	: T extends `${string}*?`
-		? { "*"?: string }
+	: T extends `${string}/*?`
+		? { "*"?: string | undefined }
 	: {};
 
 export function inject<T extends string>(route: T, values: RouteParams<T>): string;
